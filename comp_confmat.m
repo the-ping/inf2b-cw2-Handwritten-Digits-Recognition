e838fcae6c6bed87a9ee188a6bd7f7ec19d5e43c
the-ping <44267744+the-ping@users.noreply.github.com>
2019-04-04 23:10:35 +0100

    Add files via upload

--- comp_confmat.m
+++ comp_confmat.m
@@ -7,76 +7,21 @@ function [CM, acc] = comp_confmat(Ytrues, Ypreds, K)
 %   acc : accuracy (i.e. correct classification rate)
 % 
 
-
-
+N = length(Ytrues);
 CM = zeros(K,K);
-tic
-% sort Ypreds
-    [Ypreds, idx] = sort(Ypreds, 'ascend');
-    % sort Ytrues according to idx
-    Ytrues = Ytrues(idx);
-    % now each element in Ytrues match up with Ypreds'
-for k = 1:K
-    for kk = 1:K
-        % count those elements that match in Ypreds and Ytrues
-        i = 1:length(Ytrues);
-        if Ypreds(i) == Ytrues(i)
-        correctcounts = sum(Ypreds(i)==k-1); %correct classification
-        CM(k,k) = correctcounts;
-        else
-        wrongcounts = sum(Ypreds(i)==k-1 & (Ytrues(i) == kk-1));
-        CM(kk,k) = wrongcounts;
-        end
-    end
 
+for k = 1:N
+    yt = Ytrues(k) + 1;
+    yp = Ypreds(k) + 1;
+    
+    %store increase count when match
+    CM(yt, yp) = CM(yt, yp) + 1;
+    
 end
-toc
-   
-%accuracy of correct classification
-correctos = diag(CM); %extract diagonal of CM, which are the correctly predicted labels
-acc = sum(correctos)/length(Ypreds); %those correct over the total
- return;
-%     
-%     % create array of 1s and 0s, 1s to signify next element is different.
-%     difference = diff([Ypreds, max(Ypreds)+1]);
-%     % extract indices where 1s occured
-%     exindices = find([1, difference]);
-%     % calculate distances between the 1s occurences
-%     count = diff(exindices);
-%     % correseponding integers?
-%     y = Ypreds(find(difference))';
-%     
-    
-    
 
-% for k = 1:K
-%     % cum countup no. of occurences of k occuring in Ypreds
-%     
-%        for i = 1:length(Ypreds)
-%         count = sum(Ypreds(1:i) == k-1); %# of k's in Ypred
-%            for t = 1:K
-%             if Ytrues(i) == t-1 %is true label t?
-%                 %store the count at tth row of CM for digit k
-%                 CM(t,k) = count;
-%             end
-%            end
-%        end
-%        
-% end
-% CM = zeros(K,K);
-% 
-% for k = 1:K
-%        for i = 1:length(Ypreds)
-%         count = sum(Ypreds(1:i) == k-1); %# of k's in Ypred
-%            for t = 1:K
-%             if Ytrues(i) == t-1 %is true label t?
-%                 %store the count at tth row of CM for digit k
-%                 CM(t,k) = count;
-%             end
-%            end
-%        end
-%        
-% end
+%no. of correct classifications / trace = sum of diagonal elements
+acc  = trace(CM)/N;
+
 
 
 end
--- task2_1.m
+++ task2_1.m
@@ -7,6 +7,32 @@ function task2_1(Xtrain, Ytrain, Xtest, Ytest, Ks)
 %  Xtest  : N-by-D test data matrix (double)
 %  Ytest  : N-by-1 label vector (unit8) for Xtest
 %  Ks     : 1-by-L vector (integer) of the numbers of nearest neighbours in Xtrain
+tic
+[yp] = run_knn_classifier(Xtrain, Ytrain, Xtest, Ks);
+time = toc
+
+
+for K = 1:length(Ks)
+    [cm, acc] = comp_confmat(Ytest, yp(:,K), 10);
+    filename = sprintf('task2_1_cm%d.mat', Ks(K));
+    save(filename, 'cm');
+    wrongs = length(Ytest) - trace(cm);
+   
+
+%     diag(cm)
+%     sum(diag(cm))
+%     wrongs
+    
+    display(strcat('k= ' , int2str(Ks(K)))); % 1.no. of nearest neighbors
+    display(strcat('N= ', int2str(length(Xtest)))); % no. of test samples
+    display(((wrongs))); %no. of wrong classifications
+    display((acc)); %correct classification rate
+    
+    
+    
+end
+
+
 
 
 end
