b88d7cc336a609e8c015c4d153284f9030711911
the-ping <44267744+the-ping@users.noreply.github.com>
2019-04-04 15:23:28 +0100

    Add files via upload

--- task1_2.m
+++ task1_2.m
@@ -7,38 +7,21 @@ function M = task1_2(X, Y)
 % Output:
 %  M : (K+1)-by-D mean vector matrix (double)
 %      Note that M(K+1,:) is the mean vector of X.
-
-M = cell(11,1); %array to store 11 blurry iamges
-for d = 1:11
-    array = collect(X,Y,d-1); 
-    Data = cat(3,array{:}); %concatenate to get array Data
-    mu = (myMean(Data,3)); %average each element in array (uint8 type makes image blurry if Xtrn not /255)
-    M{d} = mu;
+M = zeros(0, 28*28); %array to store 11 blurry iamges
+Mfinal= zeros(28,28,0);
+for d = 1:10
+    Data = X(Y ==(d-1),:); %concatenate to get array Data
+ 
+    mu = (myMean(Data)); %average each element in array (uint8 type makes image blurry if Xtrn not /255)
+  
+    M(d,:) = mu;
+    Mfinal(:,:,d) = reshape(mu, 28, 28)';
 
 end
 
-function C = collect(X, Y, digit) %collects 10 versions of 0's
-
-        C = cell(size(X,1),1);
-        k = 1;
-        i = 1;
-        if digit == 10
-            while i<size(X,1)
-                C{k} = reshape(X(i,:), 28, 28)';
-                i = i + 1;
-                k = k + 1; 
-            end
-        end
-                
-        while i<size(X,1)   
-                if Y(i) == digit %collect images of this digit only 
-                    C{k} = reshape(X(i,:), 28, 28)';
-                    i = i + 1;
-                    k = k + 1; 
-                else
-                    i = i + 1;
-                end
-
-        end
-    end
+    M(11,:) = myMean(X);
+    Mfinal(:,:,11) = reshape(myMean(X), 28, 28)';
+    
+    
+    
 end
